9a9741e4a97dd2180b1c1c0f280b4e37a9c04d1e
JerryDot <dansinh64@gmail.com>
2018-12-01 17:33:49 +0000

    Replace list with set

--- DayOne/new_frequency_summer.py
+++ DayOne/new_frequency_summer.py
@@ -1,19 +1,17 @@
 frequency = 0
 source_input = open("numbers_to_sum.txt")
 data = source_input.readlines()
-array_of_seen_frequencies = []
+array_of_seen_frequencies = set([0])
 finished = False
 
 def check_frequency_novelty():
     global frequency
     if frequency in array_of_seen_frequencies:
-        array_of_seen_frequencies.append(frequency)
         global finished 
         finished = True
         return True
     else:
-        array_of_seen_frequencies.append(frequency)
-        array_of_seen_frequencies.sort()
+        array_of_seen_frequencies.add(frequency)
         return False
 
 def run_through_list():
