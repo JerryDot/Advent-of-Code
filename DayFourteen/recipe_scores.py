12ea2d71c89341e9007849e14dbd58fde19d9f45
JerryDot <dansinh64@gmail.com>
2019-01-12 12:34:15 +0000

    Day 14 q1

--- DayFourteen/recipe_scores.py
+++ DayFourteen/recipe_scores.py
@@ -8,34 +8,41 @@ class RecipeCalculator:
     self.elf1 = 0
     self.elf2 = 1
     self.recipes_length = 2
-    self.new_recipe_length = 0
+    self.new_recipe_value = 0
 
   def get_input_value_out(self):
     return self.input_value
 
-  def move_elf(self, elf):
-    elf = (elf +4)%self.recipes_length
-  
+  def move_elves(self):
+    self.elf1 = (self.elf1 + 1 + self.recipes[self.elf1])%self.recipes_length
+    self.elf2 = (self.elf2 + 1 + self.recipes[self.elf2])%self.recipes_length
+    
   def sum_elfs_recipes(self, elf1, elf2):
-    self.new_recipe_length = self.recipes[self.elf1] + self.recipes[self.elf2]
+    self.new_recipe_value = self.recipes[self.elf1] + self.recipes[self.elf2]
 
-  def add_new_recipes(self, new_recipe_length):
-    self.recipes.extend([int(d) for d in str(new_recipe_length)])
+  def add_new_recipes(self, new_recipe_value):
+    self.recipes.extend([int(d) for d in str(new_recipe_value)])
+    self.recipes_length += len(str(new_recipe_value))
 
   def proceed_one_line(self):
-    self.move_elf(self.elf1)
-    self.move_elf(self.elf2)
+    self.move_elves()
     self.sum_elfs_recipes(self.elf1, self.elf2)
-    self.add_new_recipes(self.new_recipe_length)
+    self.add_new_recipes(self.new_recipe_value)
 
 
-  def print_next_ten_answer(self):
-    print(self.recipes[self.input_value:(self.input_value+11)])
+  def _print_next_ten_answer(self):
+    print(self.recipes[self.input_value:(self.input_value+10)])
+    pass
 
   def get_next_ten_answer(self):
-    print(self.recipes[self.input_value:(self.input_value+11)])
-    return sum(self.recipes[self.input_value:(self.input_value+11)])
+    #self._print_next_ten_answer()
+    return ''.join(str(e) for e in self.recipes[self.input_value:(self.input_value+10)])
 
 
 
 x = RecipeCalculator(846021)
+x.add_starting_conditions()
+for count in range(x.input_value+10):
+  x.proceed_one_line()
+print(x.get_next_ten_answer())
+print(x.recipes[(x.input_value-2):(x.input_value+12)])
--- DayFourteen/test_recipe_scores.py
+++ DayFourteen/test_recipe_scores.py
@@ -20,10 +20,27 @@ def test_after_one_line():
   x.proceed_one_line()
   assert x.recipes == [3, 7, 1, 0]
 
-def test_result_after_five_recipes():
-  x = RecipeCalculator(5)
+def test_result_after_recipes_one(rec_number=5):
+  x = RecipeCalculator(rec_number)
   x.add_starting_conditions()
-  for count in range(10):
+  for count in range(rec_number+10):
     x.proceed_one_line()
-  x.print_next_ten_answer()
-  assert x.get_next_ten_answer() == 36
+  assert x.get_next_ten_answer() == "0124515891"
+
+#https://docs.pytest.org/en/latest/example/parametrize.html is worth looking into
+
+def test_result_after_recipes_two(rec_number=18):
+  x = RecipeCalculator(rec_number)
+  x.add_starting_conditions()
+  for count in range(rec_number+10):
+    x.proceed_one_line()
+  assert x.get_next_ten_answer() == "9251071085"
+
+def test_result_after_recipes_three(rec_number=2018):
+  x = RecipeCalculator(rec_number)
+  x.add_starting_conditions()
+  for count in range(rec_number+10):
+    x.proceed_one_line()
+  assert x.get_next_ten_answer() == "5941429882"
+
+
